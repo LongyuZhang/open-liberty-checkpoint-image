ee9953a4d377da18e071016e5bb974a5e971c2f0
Thomas Watson <tjwatson@us.ibm.com>
2022-07-14 10:27:23 -0500

    Use podman to build and allow older runc for running

--- examples/rest-intro/build-app.sh
+++ examples/rest-intro/build-app.sh
@@ -4,16 +4,15 @@ echo "Building rest-intro application"
 mvn package
 
 echo "Building initial application image with rest-intro-application ..."
-docker build -t rest-intro-application . || exit 1
+podman build -t rest-intro-application . || exit 1
 
 echo "Running initial application image to execute checkpoint.sh ..."
-docker run --name rest-intro-checkpoint-container --privileged --env WLP_CHECKPOINT=applications  rest-intro-application
-docker wait rest-intro-checkpoint-container || exit 1
+podman run --name rest-intro-checkpoint-container --privileged --env WLP_CHECKPOINT=applications rest-intro-application
 
 echo "Committing container rest-intro-checkpoint as image rest-intro"
-docker commit rest-intro-checkpoint-container rest-intro-checkpoint || exit 1
+podman commit rest-intro-checkpoint-container rest-intro-checkpoint || exit 1
 
 echo "Done building image rest-intro ... cleaning up"
-docker rm rest-intro-checkpoint-container
+podman rm rest-intro-checkpoint-container
 
 
--- examples/rest-intro/run-app.sh
+++ examples/rest-intro/run-app.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-podman run --cap-add=CHECKPOINT_RESTORE --cap-add=NET_ADMIN --cap-add=SYS_PTRACE --security-opt seccomp=compprofile.json -v /proc/sys/kernel/ns_last_pid:/proc/sys/kernel/ns_last_pid -p 9080:9080 --name rest-intro-restore rest-intro-checkpoint; podman rm rest-intro-restore
 
+# Use this if running with runc version 1.1.3 or higher
+# podman run --cap-add=CHECKPOINT_RESTORE --cap-add=NET_ADMIN --cap-add=SYS_PTRACE --security-opt seccomp=compprofile.json -v /proc/sys/kernel/ns_last_pid:/proc/sys/kernel/ns_last_pid -p 9080:9080 --name rest-intro-restore rest-intro-checkpoint; podman rm rest-intro-restore
+
+# Use this if running with runc version lower than 1.1.3
+# The extra --security-opt for seccomp systempaths and apparmor
+# are needed if not using the latest runc release
+# that allows mounting of ns_last_pid
+podman run --cap-add=CHECKPOINT_RESTORE --security-opt seccomp=unconfined --security-opt systempaths=unconfined --security-opt apparmor=unconfined -p 9080:9080 --name rest-intro-restore rest-intro-checkpoint; podman rm rest-intro-restore
 
 
